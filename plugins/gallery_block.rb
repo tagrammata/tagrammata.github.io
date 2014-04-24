8b0396d8be0702ed6d51d260cd537a339cc01025
Despo Pentara <despo@extractmethod.com>
2014-04-24 21:19:53 +0100

    add gallery block plugin and nano gallery library

---
+++ plugins/gallery_block.rb
@@ -0,0 +1,46 @@
+module Jekyll
+
+  class GalleryBlock < Liquid::Block
+    @gallery = nil
+
+    def initialize(tag_name, markup, tokens)
+      @markup = markup
+      super
+    end
+
+    def render(context)
+      %{<div id="gallery" class="#{@markup}"> <div class="gallery-row"> #{super} </div> </div>}
+    end
+  end
+
+  class GalleryTag < Liquid::Tag
+    @img = nil
+
+    def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+
+      if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
+        end
+        @img['class'].gsub!(/"/, '') if @img['class']
+      end
+      super
+    end
+
+    def render(context)
+      if @img
+        %{<a href='#{@img["src"]}' data-ngthumb="#{@img["src"]}"></a>}
+      else
+        "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
+        end
+    end
+  end
+end
+
+Liquid::Template.register_tag('gallery', Jekyll::GalleryBlock)
+Liquid::Template.register_tag('galleryitem', Jekyll::GalleryTag)
